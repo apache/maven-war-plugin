3102774f584c594557c01b83f0bfab18f013e7cc
Scott Kurz <skurz@us.ibm.com>
2021-05-06 12:01:07 -0400

    [MWAR-441] - Customize IT logic from copied test
    Signed-off-by: Scott Kurz <skurz@us.ibm.com>

--- src/it/MWAR-441/verify.groovy
+++ src/it/MWAR-441/verify.groovy
@@ -23,4 +23,4 @@ assert warFile.getEntry('WEB-INF/lib/mwar427-1.0-SNAPSHOT.jar') != null
 assert warFile.getEntry('index.html') != null
 
 assert warFile.getEntry('WEB-INF/lib/plexus-utils-1.4.6.jar') == null
-assert warFile.getEntry('root.html') != null // after MWAR-433, only WEB-INF/lib/ content is checked, other resources may be generated outside m-war-p
+assert warFile.getEntry('root.html') == null // after MWAR-441, this path is also removed as outdated (with the '/' config in the pom).
