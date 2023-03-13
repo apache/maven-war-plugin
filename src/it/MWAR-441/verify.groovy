85b4d60a7a4020d037b6c7623addc64665bc1624
Sylwester Lachiewicz <slachiewicz@apache.org>
2023-03-13 01:02:13 +0100

    [MWAR-457] Update plexus-utils to 3.0.24 - in its

--- src/it/MWAR-441/verify.groovy
+++ src/it/MWAR-441/verify.groovy
@@ -18,9 +18,9 @@
  */
 
 def warFile = new java.util.jar.JarFile( new File(basedir,"target/mwar427-1.0-SNAPSHOT.war"), false)
-assert warFile.getEntry('WEB-INF/lib/plexus-utils-1.4.5.jar') != null
+assert warFile.getEntry('WEB-INF/lib/plexus-utils-3.0.23.jar') != null
 assert warFile.getEntry('WEB-INF/lib/mwar427-1.0-SNAPSHOT.jar') != null
 assert warFile.getEntry('index.html') != null
 
-assert warFile.getEntry('WEB-INF/lib/plexus-utils-1.4.6.jar') == null
+assert warFile.getEntry('WEB-INF/lib/plexus-utils-3.0.24.jar') == null
 assert warFile.getEntry('root.html') == null // after MWAR-441, this path is also removed as outdated (with the '/' config in the pom).
