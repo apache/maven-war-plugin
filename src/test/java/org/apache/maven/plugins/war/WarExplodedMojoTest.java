f1c1369d740cda360368a9f8a15e992c2b332a8c
Elliotte Rusty Harold <elharo@users.noreply.github.com>
2025-10-25 12:16:40 +0000

    Remove commented code (#567)

--- src/test/java/org/apache/maven/plugins/war/WarExplodedMojoTest.java
+++ src/test/java/org/apache/maven/plugins/war/WarExplodedMojoTest.java
@@ -332,7 +332,6 @@ public class WarExplodedMojoTest extends AbstractWarExplodedMojoTest {
         File expectedWebSource2File = new File(webAppDirectory, "org/web/app/last-exile.jsp");
         // final name form is <artifactId>-<version>.<type>
         File expectedEJBArtifact = new File(webAppDirectory, "WEB-INF/lib/ejbartifact-0.0-Test.jar");
-        // File expectedEJBArtifact = new File( webAppDirectory, "WEB-INF/lib/ejbartifact-0.0-Test.jar" );
 
         assertTrue("source files not found: " + expectedWebSourceFile.toString(), expectedWebSourceFile.exists());
         assertTrue("source files not found: " + expectedWebSource2File.toString(), expectedWebSource2File.exists());
