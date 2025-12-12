b8a05feb05f386074e81e40eacd9afe154fa17a8
Sandra Parsick <sandra@parsick.dev>
2025-12-12 10:50:22 +0100

    refactor: spotless

--- src/test/java/org/apache/maven/plugins/war/WarZipTest.java
+++ src/test/java/org/apache/maven/plugins/war/WarZipTest.java
@@ -23,6 +23,7 @@ import javax.inject.Inject;
 import java.io.File;
 
 import org.apache.maven.api.plugin.testing.InjectMojo;
+import org.apache.maven.api.plugin.testing.MojoExtension;
 import org.apache.maven.api.plugin.testing.MojoParameter;
 import org.apache.maven.api.plugin.testing.MojoTest;
 import org.apache.maven.artifact.Artifact;
@@ -31,7 +32,6 @@ import org.apache.maven.plugins.war.overlay.DefaultOverlay;
 import org.apache.maven.plugins.war.stub.MavenZipProject;
 import org.apache.maven.plugins.war.stub.WarArtifactStub;
 import org.apache.maven.plugins.war.stub.ZipArtifactStub;
-import org.codehaus.plexus.util.FileUtils;
 import org.junit.jupiter.api.Test;
 
 import static org.apache.maven.api.plugin.testing.MojoExtension.getBasedir;
@@ -84,11 +84,19 @@ public class WarZipTest {
     }
 
     @InjectMojo(goal = "war", pom = "src/test/resources/unit/warziptest/war-with-zip.xml")
-    @MojoParameter(name = "classesDirectory", value = "target/test-classes/unit/warziptest/one-zip-overlay-targetPath-test-data/classes/")
-    @MojoParameter(name = "warSourceDirectory", value = "target/test-classes/unit/warziptest/one-zip-overlay-targetPath-test-data/source/")
-    @MojoParameter(name = "webXml", value = "target/test-classes/unit/warziptest/one-zip-overlay-targetPath-test-data/xml-config/web.xml")
+    @MojoParameter(
+            name = "classesDirectory",
+            value = "target/test-classes/unit/warziptest/one-zip-overlay-targetPath-test-data/classes/")
+    @MojoParameter(
+            name = "warSourceDirectory",
+            value = "target/test-classes/unit/warziptest/one-zip-overlay-targetPath-test-data/source/")
+    @MojoParameter(
+            name = "webXml",
+            value = "target/test-classes/unit/warziptest/one-zip-overlay-targetPath-test-data/xml-config/web.xml")
     @MojoParameter(name = "webappDirectory", value = "target/test-classes/unit/warziptest/one-zip-overlay-targetPath")
-    @MojoParameter(name = "outputDirectory", value = "target/test-classes/unit/warziptest/one-zip-overlay-targetPath-output")
+    @MojoParameter(
+            name = "outputDirectory",
+            value = "target/test-classes/unit/warziptest/one-zip-overlay-targetPath-output")
     @MojoParameter(name = "warName", value = "simple")
     @MojoParameter(name = "workDirectory", value = "target/test-classes/unit/warziptest/work")
     @Test
@@ -122,9 +130,15 @@ public class WarZipTest {
     }
 
     @InjectMojo(goal = "war", pom = "src/test/resources/unit/warziptest/war-with-zip.xml")
-    @MojoParameter(name = "classesDirectory", value = "target/test-classes/unit/warziptest/one-zip-overlay-skip-test-data/classes/")
-    @MojoParameter(name = "warSourceDirectory", value = "target/test-classes/unit/warziptest/one-zip-overlay-skip-test-data/source/")
-    @MojoParameter(name = "webXml", value = "target/test-classes/unit/warziptest/one-zip-overlay-skip-test-data/xml-config/web.xml")
+    @MojoParameter(
+            name = "classesDirectory",
+            value = "target/test-classes/unit/warziptest/one-zip-overlay-skip-test-data/classes/")
+    @MojoParameter(
+            name = "warSourceDirectory",
+            value = "target/test-classes/unit/warziptest/one-zip-overlay-skip-test-data/source/")
+    @MojoParameter(
+            name = "webXml",
+            value = "target/test-classes/unit/warziptest/one-zip-overlay-skip-test-data/xml-config/web.xml")
     @MojoParameter(name = "webappDirectory", value = "target/test-classes/unit/warziptest/one-zip-overlay-skip")
     @MojoParameter(name = "outputDirectory", value = "target/test-classes/unit/warziptest/one-zip-overlay-skip-output")
     @MojoParameter(name = "warName", value = "simple")
@@ -143,9 +157,15 @@ public class WarZipTest {
     }
 
     @InjectMojo(goal = "war", pom = "src/test/resources/unit/warziptest/war-with-zip.xml")
-    @MojoParameter(name = "classesDirectory", value = "target/test-classes/unit/warziptest/one-zip-overlay-skip-test-data/classes/")
-    @MojoParameter(name = "warSourceDirectory", value = "target/test-classes/unit/warziptest/one-zip-overlay-skip-test-data/source/")
-    @MojoParameter(name = "webXml", value = "target/test-classes/unit/warziptest/one-zip-overlay-skip-test-data/xml-config/web.xml")
+    @MojoParameter(
+            name = "classesDirectory",
+            value = "target/test-classes/unit/warziptest/one-zip-overlay-skip-test-data/classes/")
+    @MojoParameter(
+            name = "warSourceDirectory",
+            value = "target/test-classes/unit/warziptest/one-zip-overlay-skip-test-data/source/")
+    @MojoParameter(
+            name = "webXml",
+            value = "target/test-classes/unit/warziptest/one-zip-overlay-skip-test-data/xml-config/web.xml")
     @MojoParameter(name = "webappDirectory", value = "target/test-classes/unit/warziptest/one-zip-overlay-skip")
     @MojoParameter(
             name = "outputDirectory",
@@ -171,33 +191,10 @@ public class WarZipTest {
         assertZipContentNotHere(mojo.getWebappDirectory());
     }
 
-    private void configureMojo(
-            WarMojo mojo, File classesDir, File webAppSource, File webAppDirectory, File xmlSource, Overlay overlay)
-            throws Exception {
-        configureMojo(mojo, classesDir, webAppSource, webAppDirectory, xmlSource);
-        mojo.addOverlay(overlay);
-    }
-
-    private void configureMojo(WarMojo mojo, File classesDir, File webAppSource, File webAppDirectory, File xmlSource)
-            throws Exception {
-        WarArtifactStub warArtifact = new WarArtifactStub(getBasedir());
-        MavenZipProject project = new MavenZipProject();
-        project.setArtifact(warArtifact);
-        project.getArtifacts().add(buildZipArtifact());
-        mojo.setProject(project);
-        mojo.setClassesDirectory(classesDir);
-        mojo.setWarSourceDirectory(webAppSource);
-        mojo.setWebappDirectory(webAppDirectory);
-        mojo.setWebXml(new File(xmlSource, "web.xml"));
-    }
-
     private Artifact buildZipArtifact() throws Exception {
-        File zipFile = new File(getTestDirectory(), "foobar.zip");
+        File zipFile =
+                new File(new File(MojoExtension.getBasedir(), "target/test-classes/unit/warziptest"), "foobar.zip");
         return new ZipArtifactStub("src/test/resources/unit/warziptest", artifactHandler, zipFile);
-    }
-
-    private File getTestDirectory() {
-        return new File(getBasedir(), "target/test-classes/unit/warziptest");
     }
 
     private void assertZipContentNotHere(File webAppDirectory) {
@@ -212,101 +209,5 @@ public class WarZipTest {
         File bar = new File(barDirectory, "bar.txt");
         assertFalse(bar.exists(), "bar/bar.txt exists");
         assertFalse(bar.isFile(), "bar/bar.txt is a file");
-    }
-
-    /**
-     * create an isolated xml dir
-     *
-     * @param id The id.
-     * @param xmlFiles array of xml files.
-     * @return The created file.
-     * @throws Exception in case of errors.
-     */
-    private File createXMLConfigDir(String id, String[] xmlFiles) throws Exception {
-        File xmlConfigDir = new File(getTestDirectory(), "/" + id + "-test-data/xml-config");
-        File xmlFile;
-
-        createDir(xmlConfigDir);
-
-        if (xmlFiles != null) {
-            for (String o : xmlFiles) {
-                xmlFile = new File(xmlConfigDir, o);
-                createFile(xmlFile);
-            }
-        }
-
-        return xmlConfigDir;
-    }
-
-    /**
-     * Returns the webapp source directory for the specified id.
-     *
-     * @param id the id of the test
-     * @return the source directory for that test
-     * @throws Exception if an exception occurs
-     */
-    private File getWebAppSource(String id) throws Exception {
-        return new File(getTestDirectory(), "/" + id + "-test-data/source");
-    }
-
-    /**
-     * create an isolated web source with a sample jsp file
-     *
-     * @param id The id.
-     * @param createSamples Create example files yes or no.
-     * @return The created file.
-     * @throws Exception in case of errors.
-     */
-    private File createWebAppSource(String id, boolean createSamples) throws Exception {
-        File webAppSource = getWebAppSource(id);
-        if (createSamples) {
-            File simpleJSP = new File(webAppSource, "pansit.jsp");
-            File jspFile = new File(webAppSource, "org/web/app/last-exile.jsp");
-
-            createFile(simpleJSP);
-            createFile(jspFile);
-        }
-        return webAppSource;
-    }
-
-    private File createWebAppSource(String id) throws Exception {
-        return createWebAppSource(id, true);
-    }
-
-    /**
-     * create a class directory with or without a sample class
-     *
-     * @param id The id.
-     * @param empty true to create a class files false otherwise.
-     * @return The created class file.
-     * @throws Exception in case of errors.
-     */
-    private File createClassesDir(String id, boolean empty) throws Exception {
-        File classesDir = new File(getTestDirectory() + "/" + id + "-test-data/classes/");
-
-        createDir(classesDir);
-
-        if (!empty) {
-            createFile(new File(classesDir + "/sample-servlet.clazz"));
-        }
-
-        return classesDir;
-    }
-
-    private void createDir(File dir) {
-        if (!dir.exists()) {
-            assertTrue(dir.mkdirs(), "can not create test dir: " + dir.toString());
-        }
-    }
-
-    private void createFile(File testFile, String body) throws Exception {
-        createDir(testFile.getParentFile());
-        FileUtils.fileWrite(testFile.toString(), body);
-
-        assertTrue(testFile.exists(), "could not create file: " + testFile);
-    }
-
-    private void createFile(File testFile) throws Exception {
-        createFile(testFile, testFile.toString());
     }
 }
